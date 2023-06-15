20e35930b77d7c865dd3c92687bb64cad9bce119
Marlon Colato <mcolato2104@gmail.com>
2023-06-15 13:25:45 -0400

    Sprint 6 challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./components/Character";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [people, setPeople] = useState([]);
+ 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setPeople(res.data);
+      })
+      .catch(err => console.err(err));
+  },[])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -11,8 +23,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {people.map((char, index) => (
+        <Character key={index} char={char}/>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// https://swapi.dev/api/people/`
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+
+
+const Character = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1 style={{color: '#160D08',}}>{props.char.name}</h1>
+        </div>
+    )
+}
+
+export default Character;
